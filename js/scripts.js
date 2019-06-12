6fd082ac5dc6c1e9520235506d80fd7af0538101
Brendon Tse and Lake Bramlett <brendon@>
2019-06-12 11:04:25 -0700

    add functionality to move first consonant in word and append ay

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,63 @@
+// /////business logic//////
+//
+//
+// function translate() {
+//   var phrase = $('.user-input').val();
+//   var vowels = ['a','e','i','o','u'];
+//   console.log(phrase);
+//   console.log(typeof phrase);
+//   if (phrase === 'a' || phrase === 'e' || phrase === 'i' || phrase === 'o' || phrase === 'u') {
+//     $('.output').show()
+//     $('.output .results').show().append(phrase + 'ay');
+//   } else if (vowels.includes(phrase.charAt(0)) === 'a' || phrase.charAt(0) === 'e' || phrase.charAt(0) === 'i' || phrase.charAt(0) === 'o' || phrase.charAt(0) === 'u') {
+//     $('.output').show();
+//     $('.output .results').show().append(phrase + 'way');
+//   } else if (phrase.charAt(0) === )
+//
+// };//function Translate
+//
+//
+// /////ui logic//////
+//
+//
+// $(document).ready(function() {
+//   console.log('jquery is enabled');
+//   $('button').click(function() {
+//     $('.output .results').empty();
+//     translate();
+//   });//click event
+//
+//
+//
+//
+//
+// });//jquery
+
+
+
+
 /////business logic//////
 
 
 function translate() {
   var phrase = $('.user-input').val();
   var vowels = ['a','e','i','o','u'];
+  var consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'];
   console.log(phrase);
   console.log(typeof phrase);
   if (phrase === 'a' || phrase === 'e' || phrase === 'i' || phrase === 'o' || phrase === 'u') {
     $('.output').show()
     $('.output .results').show().append(phrase + 'ay');
-  } else if (phrase.charAt(0) === 'a' || phrase.charAt(0) === 'e' || phrase.charAt(0) === 'i' || phrase.charAt(0) === 'o' || phrase.charAt(0) === 'u') {
-    $('.output').show()
+  } else if (vowels.includes(phrase.charAt(0))) {
+    $('.output').show();
     $('.output .results').show().append(phrase + 'way');
-  }
+  } else if (consonants.includes(phrase.charAt(0))) {
+    var firstConsonant = phrase.charAt(0);
+    console.log('sliced phrase: ' + phrase.slice(1));
+    $('.output').show();
+    $('.output .results').show().append(phrase.slice(1) + firstConsonant + 'ay');
+
+  };
 
 };//function Translate
 
