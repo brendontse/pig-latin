71c0e4de7f4000543ab6699857e0bbff327ed8a0
Brendon Tse and Lake Bramlett <brendon@>
2019-06-12 13:46:22 -0700

    add multiple consonants functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,30 @@
-// /////business logic//////
-//
-//
-// function translate() {
-//   var phrase = $('.user-input').val();
-//   var vowels = ['a','e','i','o','u'];
-//   console.log(phrase);
-//   console.log(typeof phrase);
-//   if (phrase === 'a' || phrase === 'e' || phrase === 'i' || phrase === 'o' || phrase === 'u') {
-//     $('.output').show()
-//     $('.output .results').show().append(phrase + 'ay');
-//   } else if (vowels.includes(phrase.charAt(0)) === 'a' || phrase.charAt(0) === 'e' || phrase.charAt(0) === 'i' || phrase.charAt(0) === 'o' || phrase.charAt(0) === 'u') {
-//     $('.output').show();
-//     $('.output .results').show().append(phrase + 'way');
-//   } else if (phrase.charAt(0) === )
-//
-// };//function Translate
-//
-//
-// /////ui logic//////
-//
-//
-// $(document).ready(function() {
-//   console.log('jquery is enabled');
-//   $('button').click(function() {
-//     $('.output .results').empty();
-//     translate();
-//   });//click event
-//
-//
-//
-//
-//
-// });//jquery
+///business logic//////
 
-
-
-
-/////business logic//////
 
 
 function translate() {
   var phrase = $('.user-input').val();
   var vowels = ['a','e','i','o','u'];
   var consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'];
+  var vowelIndexes = [];
+  function getFirstVowel() {
+    vowels.forEach(function(x) {
+      if (phrase.indexOf(x) >= 1) {
+      vowelIndexes.push(phrase.indexOf(x));
+      vowelIndexes.sort();
+      console.log('index of vowels is: ' + vowelIndexes);
+
+      };
+    });
+  };
+
+  function getConsonants() {
+    for (i = 0; i < vowelIndexes[0]; i++ ) {
+    firstConsonants += phrase.charAt(i);
+    console.log('first consonants: ' + firstConsonants);
+    };
+  };
+
   console.log(phrase);
   console.log(typeof phrase);
   if (phrase === 'a' || phrase === 'e' || phrase === 'i' || phrase === 'o' || phrase === 'u') {
@@ -52,15 +34,38 @@ function translate() {
     $('.output').show();
     $('.output .results').show().append(phrase + 'way');
   } else if (consonants.includes(phrase.charAt(0))) {
-    var firstConsonant = phrase.charAt(0);
-    console.log('sliced phrase: ' + phrase.slice(1));
+    var firstConsonants = '';
+    var indexOfFirstVowel = 0;
+    getFirstVowel();
+    getConsonants();
+    console.log('sliced phrase: ' + phrase.slice(vowelIndexes[0]));
     $('.output').show();
-    $('.output .results').show().append(phrase.slice(1) + firstConsonant + 'ay');
-
+    $('.output .results').show().append(phrase.slice(vowelIndexes[0]) + firstConsonants + 'ay');
   };
 
 };//function Translate
 
+
+
+// function translate() {
+//   var phraseArray = Array.from($('.user-input').val());
+//   var vowels = ['a','e','i','o','u'];
+//   var consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'];
+//   console.log(phraseArray);
+//   console.log(typeof phraseArray);
+//   if (phraseArray === 'a' || phraseArray === 'e' || phraseArray === 'i' || phraseArray === 'o' || phraseArray === 'u') {
+//     $('.output').show()
+//     $('.output .results').show().append(phraseArray + 'ay');
+//   } else if (vowels.includes(phraseArray[0])) {
+//     $('.output').show();
+//     $('.output .results').show().append(phraseArray + 'way');
+//   } else if (consonants.includes(phrase.charAt(0))) {
+//     var firstConsonant = phrase.charAt(0);
+//     console.log('sliced phrase: ' + phrase.slice(1));
+//     $('.output').show();
+//     $('.output .results').show().append(phrase.slice(1) + firstConsonant + 'ay');
+//   };
+// };
 
 /////ui logic//////
 
