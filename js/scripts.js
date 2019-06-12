cb2a4413c17d904c8b8666379f1580d48fb609e8
Brendon Tse and Lake Bramlett <brendon@>
2019-06-12 09:44:41 -0700

    initial project files and structure

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+//business logic
+
+
+
+//ui logic
+$(document).ready(function() {
+  console.log('jquery is enabled');
+
+  $('button').click(function() {
+    var phrase = $('.user-input').val();
+    console.log(phrase);
+
+  });//click event
+
+
+
+
+
+});//jquery
