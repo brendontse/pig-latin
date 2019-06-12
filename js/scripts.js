42bb0d6e1e012d058647da63351e324d75e46107
Brendon Tse and Lake Bramlett <brendon@>
2019-06-12 10:15:08 -0700

    add first spec condition, add ay to single vowel words

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,27 @@
-//business logic
+/////business logic//////
 
 
+function translate() {
+  var phrase = $('.user-input').val();
+  var vowels = ['a','e','i','o','u'];
+  console.log(phrase);
+  console.log(typeof phrase);
+  if (phrase === 'a' || phrase === 'e' || phrase === 'i' || phrase === 'o' || phrase === 'u') {
+    $('.output').show()
+    $('.output .results').show().append(phrase + 'ay');
+  };//if statement
 
-//ui logic
+};//function Translate
+
+
+/////ui logic//////
+
+
 $(document).ready(function() {
   console.log('jquery is enabled');
-
   $('button').click(function() {
-    var phrase = $('.user-input').val();
-    console.log(phrase);
-
+    $('.output .results').empty();
+    translate();
   });//click event
 
 
